4172489ca96d1932d2d07b00311906d36a6dac23
Ajith Kumar <Ajith Kumar@AJITHKUMARN-160>
2025-08-29 22:07:22 +0530

    Minmal Api

---
+++ UniHub.Api/MinimalApiEndpoints/UtilityEndpoints.cs
@@ -0,0 +1,19 @@
+﻿namespace UniHub.Api.MinimalApiEndpoints
+{
+    public static class UtilityEndpoints
+    {
+        public static void MapUtilityEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/ping", () => Results.Ok("Pong ✅"))
+               .WithName("Ping")
+               .WithTags("Utility");
+
+            app.MapGet("/time", () =>
+            {
+                return Results.Ok(new { ServerTime = DateTime.UtcNow });
+            })
+            .WithName("ServerTime")
+            .WithTags("Utility");
+        }
+    }
+}
---
+++ UniHub.Api/MinimalApiRegistration.cs
@@ -0,0 +1,13 @@
+﻿using UniHub.Api.MinimalApiEndpoints;
+
+namespace UniHub.Api
+{
+    public static class MinimalApiRegistration
+    {
+        public static void RegisterMinimalApis(this WebApplication app)
+        {
+            // Call each endpoint group here
+            app.MapUtilityEndpoints();
+        }
+    }
+}
