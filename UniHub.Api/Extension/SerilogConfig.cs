a1e77d1537379d28b27d04e734acd3bfafc48ea1
Ajith Kumar <Ajith Kumar@AJITHKUMARN-160>
2025-08-30 12:29:03 +0530

    Commit

---
+++ UniHub.Api/Extension/SerilogConfig.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using Serilog.Filters;
+
+namespace UniHub.Api.Extenstion
+{
+    public static class SerilogConfig
+    {
+        public static void LogConfig()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+
+                // Write ONLY logs where LogType is null/empty → General
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByExcluding(Matching.WithProperty<string>("LogType", v => !string.IsNullOrEmpty(v)))
+                    .WriteTo.File(
+                        "Logs/General/log-.txt",
+                        rollingInterval: RollingInterval.Day,
+                        retainedFileCountLimit: 7
+                    )
+                )
+
+                // Conditional per LogType (non-empty ones)
+                .WriteTo.Map(
+                    "LogType",
+                    (logType, wt) =>
+                    {
+                        if (!string.IsNullOrEmpty(logType))
+                        {
+                            wt.File(
+                                $"Logs/{logType}/log-.txt",
+                                rollingInterval: RollingInterval.Day,
+                                retainedFileCountLimit: 7
+                            );
+                        }
+                    }
+                )
+
+                .CreateLogger();
+        }
+    }
+}
